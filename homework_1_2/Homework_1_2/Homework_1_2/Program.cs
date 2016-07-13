32a15505fc4e53aa7c3cd96e44dd849bf2f40489
sergeygrakhov <garrykiev@gmail.com>
2016-07-13 14:11:43 +0300

    homework1 added

---
+++ homework_1_2/Homework_1_2/Homework_1_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str = "hello world";
+
+            foreach (char symbol in str)
+            {
+                int dec = Convert.ToInt32(symbol);
+                string hex = String.Format("{0:X}", dec);
+                Console.WriteLine("Char {0}: dec {1,3}, hex {2,3} ", symbol, dec, hex);
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
