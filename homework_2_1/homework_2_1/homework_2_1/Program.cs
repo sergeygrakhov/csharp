3faab11c3db49cf8a7f4a8402baff6c23262ee83
sergeygrakhov <garrykiev@gmail.com>
2016-07-16 01:22:03 +0300

    homework 3 added

--- homework_2_1/homework_2_1/homework_2_1/Program.cs
+++ homework_2_1/homework_2_1/homework_2_1/Program.cs
@@ -17,30 +17,30 @@ namespace homework_2_1
   			System.Boolean _bOne = false;
   			System.Boolean _bTwo = true;  			
   			Console.SetWindowSize(61,32);
-  			Console.WriteLine("Arithmetic operations with numbers 123 and 42:\n" +
+  			Console.WriteLine("Arithmetic operations with numbers 123 and 42:\r\n" +
   			                  "+: {0}\n" +
   			                  "-: {1}\n" +
   			                  "*: {2}\n" +
   			                  "/: {3}\n" +
   			                  "%: {4}\n", _iOne + _iTwo,_iOne - _iTwo,_iOne * _iTwo,_iOne / _iTwo,_iOne % _iTwo);
-  			Console.WriteLine("Comparsion operations with numbers 123 and 42:\n" +
+  			Console.WriteLine("Comparsion operations with numbers 123 and 42:\r\n" +
   			                  ">: {0}\n" +
   			                  "<: {1}\n" +
   			                  ">=: {2}\n" +
   			                  "<=: {3}\n" +
   			                  "!=: {4}\n" +
   			                  "==: {5}\n", _iOne > _iTwo,_iOne < _iTwo,_iOne >= _iTwo,_iOne <= _iTwo,_iOne != _iTwo,_iOne == _iTwo );
-  			Console.WriteLine("Logical operations with false and true:\n" +
+  			Console.WriteLine("Logical operations with false and true:\r\n" +
   			                  "&&: {0}\n" +
   			                  "||: {1}\n" +
   			                  "^: {2}\n",  _bOne && _bTwo,_bOne || _bTwo,_bOne ^ _bTwo);
-  			Console.WriteLine("Bit operations with numbers 123 and 42:\n" +
+  			Console.WriteLine("Bit operations with numbers 123 and 42:\r\n" +
   			                  "&: {0}\n" +
   			                  "|: {1}\n" +
   			                  "^: {2}\n" +
   			                  "<<: {3}\n" +
   			                  ">>: {4}\n", _iOne & _iTwo,_iOne | _iTwo,_iOne ^ _iTwo,_iOne << _iTwo,_iOne >> _iTwo);
-  			Console.WriteLine("Operations with strings {0} and {1}\n" +
+  			Console.WriteLine("Operations with strings {0} and {1}\r\n" +
   			                  "Concatenation: {2}\n" +
   			                  "Concatenation witn numbers and booleans: {3}",_sOne, _sTwo, _sOne + ' ' + _sTwo, _sOne +_iOne+_bOne+ _sTwo );
   			Console.WriteLine("Press any key to exit ...");
---
+++ homework_3/homework_3/homework_3/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Byte _bWrongInputCount = 0;
+            Boolean _userFind = false;
+            Boolean _passwordCorrect = false;
+            String _userName = default(String);
+            String _userRole = default(String);
+            String _userEnteredPassword = default(String);
+            int _index = 0;
+            int _iUserQwantity = 0;
+            String[,] _arrayUsers = new String[,]
+                {
+                    {"Admin","Superman","kriptonit"},
+                    {"Admin","Batman","gotham"},
+                    {"Admin","Ironman","avenger"},
+                    {"Moderator","Andrew","dotnet"},
+                    {"Moderator","Jeffrey","csharp"},
+                    {"User","Ivan","qwerty"},
+                    {"User","Peter","helloworld"},
+                    {"User","Nick","chupachups"},
+                    {"User","Russel","delldell"},
+                    {"User","John","snow"},
+                };
+            
+            Console.Write("Login: ");
+            _userName = Console.ReadLine();
+            for (int i = 0; i < _arrayUsers.GetLength(0); i++)
+            {
+                if (_userName == _arrayUsers[i, 1])
+                {
+                    _index = i;
+                    _userFind = true;
+                    break;
+                }
+            }
+            if (_userFind)
+            {
+                while (_bWrongInputCount < 3)
+                {
+                    Console.Write("Password: ");
+                    
+                    _userEnteredPassword = Console.ReadLine();
+                    if (_userEnteredPassword == _arrayUsers[_index,2])
+                    {
+                        _userRole = _arrayUsers[_index, 0];
+                        _passwordCorrect = true;
+                        break;
+                    }
+                    else
+                    {
+                        _bWrongInputCount++;
+                        if (_bWrongInputCount == 3)
+                        {
+                            Console.WriteLine("You reach max tries, program will be finished!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Incorrect password!");
+                        }
+                    }
+                }
+                if(_passwordCorrect)
+                {
+                    switch (_userRole)
+                    {
+                        case "Admin":
+                            Console.WriteLine("List of all users:");
+                            for (int i = 0; i < _arrayUsers.GetLength(0); i++)
+                            {
+                                Console.WriteLine("{0,3}: Username {1,10}, Password {2,10}, Role {3,10} ",i+1, _arrayUsers[i,1], _arrayUsers[i, 2], _arrayUsers[i, 0]);
+                            }
+                            break;
+                        case "Moderator":
+                            Console.WriteLine("Quantity of all users: {0} ",_arrayUsers.GetLength(0));
+                            break;
+                        case "User":
+                            Console.WriteLine("Name of users:");
+                            for (int i = 0; i < _arrayUsers.GetLength(0); i++)
+                            {
+                                if(_arrayUsers[i,0] == "User")
+                                {
+                                    Console.WriteLine("{0,3}: {1,10}",_iUserQwantity+1, _arrayUsers[i, 1]);
+                                    _iUserQwantity++;
+                                }
+                            }
+                            Console.WriteLine("Quantity: {0}", _iUserQwantity);
+                            break;
+                    }
+                }
+            }
+            else
+                {
+                Console.WriteLine("User with entered login not found!!!");
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
